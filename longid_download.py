b09da279a82e60d3a9594a3283acc7442c536c54
Josefson Fraga <josefson@users.noreply.github.com>
2015-08-28 16:01:57 -0400

    Logging system, new exceptions, loops fixed

--- longid_download.py
+++ longid_download.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# Git logging
 """
 This file is responsible to download the curriculum xml
 """
 import re
+import sys
+import logging
 import requests
 from PIL import Image
 from captcha import Captcha
@@ -13,11 +14,17 @@ from selenium.webdriver.common.by import By
 from multiprocessing import Process, current_process
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
 
 CORES = 2
 SHORT_ID_FILE = 'short_ids.csv'   # Input file.
 LONG_ID_FILE = 'long_ids.csv'   # Output file.
+LOG_FILENAME = 'log.txt'
+
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.basicConfig(format=FORMAT, filename=LOG_FILENAME, level=logging.INFO)
 
 def short_ids(short_id_file):
     """This function expects a short_id file and returns a list with all
@@ -39,7 +46,7 @@ def short_ids(short_id_file):
     short_id_list = short_id_list
     return short_id_list
 
-def split_list(alist, CORES):
+def split_list(alist):
     """Split a list of intervals in n parts in order to be
     multiprocessed."""
     wanted_parts = CORES
@@ -189,90 +196,126 @@ def cv_view(inf_cap_req):
 def download_cv(driver, long_id):
     """Given a selenium webdriver and a long_id, it downloads the related xml
     curriculum."""
-    # pname = current_process().name
+    pname = current_process().name
     long_url = 'http://buscatextual.cnpq.br/buscatextual/download.do?metodo='\
                'apresentar&idcnpq=' + long_id
     input_locator = 'informado'
     captcha_locator = 'image_captcha'
     while True:
-        # print '{}download_cv: loading long_url'.format(pname)
+        logging.info('%s-download_cv: Loading %s', pname, long_url)
         driver.get(long_url)
-        # print '{}download_cv: waiting for it to load...'.format(pname)
+        logging.info('%s-download_cv: Locating captcha element...', pname)
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.ID,
                                                              captcha_locator)))
-        # print '{}download_cv: cracking captcha...'.format(pname)
+        logging.info('%s-download_cv: Cracking captcha...', pname)
         code = crack_captcha(driver)
         if len(code) != 4:
-            # print '{}download_cv: wrong captcha: {}'.format(pname, code)
+            logging.info('%s-download_cv: Wrong captcha length: %s. Trying '\
+                         'again...', pname, code)
             continue
         if len(code) == 4:
-            # print '{}download_cv: captcha: {}'.format(pname, code)
+            logging.info('%s-download_cv: Right captcha: %s', pname, code)
             break
-    # print '{}download_cv: entering captcha: {}'.format(pname, code)
+    logging.info('%s-download_cv: Entering captcha: %s', pname, code)
     element = driver.find_element(By.ID, input_locator)
     element.clear()
     element.send_keys(code + '\n')
-    # print '{}S[MaS[MaSdownload_cv: waiting for download...'.format(pname)
+    logging.info('%s-download_cv: Waiting for download...', pname)
     wait = WebDriverWait(driver, 10)
     element = wait.until(EC.invisibility_of_element_located((By.ID,
                                                              captcha_locator)))
 
 def worker(short_id_list):
     """Given a list of short_ids it iterates over them to get the long_id."""
-    driver = LattesDriver().get_driver()
     pname = current_process().name
+    logging.info('%s: Started', pname)
+    try:
+        driver = LattesDriver().get_driver()
+    except WebDriverException, error:
+        print error
+        sys.exit('Failed to initialize selenium drivers. Calm down, sometimes '\
+                 'this happens, please try again. If this error persists' \
+                 'check if the Browser version is compatible with selenium.')
     output_file = open(LONG_ID_FILE, 'a')
     for count, short_id in enumerate(short_id_list):
         while True:
-            img_req = captcha_image(get_short_url(short_id))
+            while True:
+                try:
+                    logging.info('%s- Getting captcha and session for CVPAGE',
+                                 pname)
+                    img_req = captcha_image(get_short_url(short_id))
+                    break
+                except requests.exceptions.Timeout as terror:
+                    logging.info('%s-[Loop-%s]=> Timeout: %s\nTrying again...',
+                                 pname, count+1, terror)
+                    continue
+                except requests.exceptions.ConnectionError as cerror:
+                    logging.info('%s-[Loop-%s]=> Connection Error: %s\nTrying '
+                                 'again...', pname, count+1, cerror)
+                    continue
+                except requests.exceptions.RequestException as rerror:
+                    logging.info('%s-[Loop-%s]=> Request Error: %s\nTrying '
+                                 'again...', pname, count+1, rerror)
+                    continue
             image_name = 'temp_' + pname + '.png'
             open(image_name, 'wb').write(img_req.content)
             code = Captcha(image_name).get_text().upper()
             if len(code) != 4:
-                # print '{}worker: code != 4: {}'.format(pname, code)
+                logging.info('%s- Wrong code length: %s', pname, code)
                 continue
             elif len(code) == 4:
-                # print '{}worker: code == 4: {}'.format(pname, code)
+                logging.info('%s- Right code length: %s', pname, code)
                 try:
                     inf_req = inform_captcha(img_req, code)
                     post_do(inf_req)
                     cv_req = cv_view(inf_req)
                 except requests.exceptions.Timeout as terror:
-                    print '{}-[Loop-{}]=> Timeout: {}\nTrying'\
-                            'again...'.format(pname, count+1, terror)
+                    logging.info('%s-[Loop-%s]=> Timeout: %s\nTrying again...',
+                                 pname, count+1, terror)
                     continue
                 except requests.exceptions.ConnectionError as cerror:
-                    print '{}-[Loop-{}]=> Connection Error: {}\nTrying'\
-                            'again...'.format(pname, count+1, cerror)
+                    logging.info('%s-[Loop-%s]=> Connection Error: %s\nTrying '
+                                 'again...', pname, count+1, cerror)
                     continue
                 except requests.exceptions.RequestException as rerror:
-                    print '{}-[Loop-{}]=> RequestError: {}\nTrying'\
-                            'again...'.format(pname, count+1, rerror)
+                    logging.info('%s-[Loop-%s]=> Request Error: %s\nTrying '
+                                 'again...', pname, count+1, rerror)
                     continue
                 if verify_page(cv_req) == 'CVPAGE':
-                    # print '{}worker: inside CVPAGE, getting long_id'.format(pname)
+                    logging.info('%s- Inside CVPAGE, scraping long_id', pname)
                     long_id = scrap_long_id(cv_req)
-                    print '{}-[{}/{}]=> short_id: {}  |  long_id: {}'.format(
-                        pname, count+1, len(short_id_list), short_id, long_id)
+                    logging.info('%s-[%s/%s]=> short_id: %s | long_id: %s',
+                                 pname, count+1, len(short_id_list), short_id,
+                                 long_id)
                     output_file.write(short_id + ' | ' + long_id + '\n')
-                    # print '{}worker: Downloading the cv for long_id: {}'.format(pname, long_id)
-                    try:
-                        download_cv(driver, long_id)
-                    except:
-                        continue
+                    output_file.flush()
+                    while True:
+                        try:
+                            logging.info('%s- Downloading the CV for long_id: %s',
+                                         pname, long_id)
+                            download_cv(driver, long_id)
+                            logging.info('%s- Download finished!', pname)
+                            break
+                        except Exception, derror:
+                            print derror
+                            logging.info('%s- Download failed. Trying again',
+                                         pname)
+                            continue
                     break
                 elif verify_page(cv_req) == 'NOTFOUND':
-                    print '{}-[{}/{}]=> short_id: {}  |  long_id: NOTFOUND'.format(
-                        pname, count+1, len(short_id_list), short_id)
+                    logging.info('%s-[%s/%s]=> short_id: %s | long_id: '\
+                                 'NOTFOUND', pname, count+1,
+                                 len(short_id_list), short_id)
                     output_file.write(short_id + ' | NOTFOUND\n')
+                    output_file.flush()
                     break
     driver.close()
 
 def main():
     """Main function, which controls the workflow of the program."""
     short_id_list = short_ids(SHORT_ID_FILE)
-    splited_lists = split_list(short_id_list[0:10], CORES)
+    splited_lists = split_list(short_id_list[0:10])
     for splited_list in range(len(splited_lists)):
         temp = (splited_lists[splited_list],)
         process = Process(target=worker, args=temp)
